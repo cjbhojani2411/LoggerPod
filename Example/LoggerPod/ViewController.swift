4dde410a02347aac56c69e0eea71d4d6876a65c4
Chirag Bhojani <chirag.bhojani@pardypanda.com>
2024-04-09 12:45:54 +0530

    Add linePhone pod to podSpec 1

--- Example/LoggerPod/ViewController.swift
+++ Example/LoggerPod/ViewController.swift
@@ -7,19 +7,27 @@
 //
 
 import UIKit
-import LoggerPod
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var leftSeprator: UIView!
+    @IBOutlet weak var rightSeprator: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        let logger = Logger()
-        logger.printLog()
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        let leftGradient = CAGradientLayer()
+        leftGradient.frame = CGRect(x: 0, y: 0, width: 200, height: 1)
+        leftGradient.colors = [UIColor.black, UIColor.yellow]
+        leftGradient.startPoint = CGPoint(x: 1, y: 0)
+        leftGradient.endPoint = CGPoint(x: 0, y: 0)
+        leftGradient.locations = [0, 1]
+        leftSeprator.layer.addSublayer(leftGradient)
+        
+        let rightGradient = CAGradientLayer()
+        rightGradient.frame = CGRect(x: 0, y: 0, width: rightSeprator.frame.size.width, height: 1)
+        rightGradient.colors = [UIColor.black, UIColor.yellow]
+        rightGradient.endPoint = CGPoint(x: 1, y: 0)
+        rightGradient.startPoint = CGPoint(x: 0, y: 0)
+        rightGradient.locations = [0, 1]
+        rightSeprator.layer.addSublayer(rightGradient)
     }
 }
-
--- LoggerPod.podspec
+++ LoggerPod.podspec
@@ -8,9 +8,9 @@
 
 Pod::Spec.new do |s|
   s.name             = 'LoggerPod'
-  s.version          = '0.1.0'
+  s.version          = '0.1.1'
   s.summary          = 'This is my first pod for logs.'
-
+  s.swift_version = "5.0"
 # This description is used to generate tags and improve search results.
 #   * Think: What does it do? Why did you write it? What is the focus?
 #   * Try to keep it short, snappy and to the point.
@@ -36,5 +36,5 @@ Pod::Spec.new do |s|
 
   # s.public_header_files = 'Pod/Classes/**/*.h'
   # s.frameworks = 'UIKit', 'MapKit'
-  # s.dependency 'AFNetworking', '~> 2.3'
+   s.dependency 'linphone-sdk', '~>5.3.4'
 end
